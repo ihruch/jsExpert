5751b31446df97dbc167f3ec78621409df7163ac
ihruch <ihruch@gmail.com>
2018-02-04 23:30:43 +0200

    add moment js

--- less_17/homeWork_17/js/main.js
+++ less_17/homeWork_17/js/main.js
@@ -1,63 +1,68 @@
 var btn = document.getElementById("play");
 
 
-// вырезать 6 эл-т
+// вырезать  эл-т
 function deleteElementArray(arr, numberItem){
-    return arr.splice(numberItem, 1);
+    return arr.splice(--numberItem, 1);
 }
 
 // Создание нового массива без ключа Id
 let createNewArray =(arr) => {
     let newArr = [];
-       arr.forEach(function(item){
+       arr.forEach( (item) =>
            newArr.push({
                url: item.url,
                name: item.name,
                params: item.params,
                description: item.description,
                date: item.date
-           });//end push
-    });
+           })//end push
+        );
     return newArr;
-}
+};
 
 // обрезает строку до 15 символов и в конце добавляет троеточик.
 let addThreePoint = (str) => (str.length > 15)? str.slice(0,15) + '...' : str;
 
 // ф-ция отдеает нужную дату.
 let setDateItem = (millisec) => {
-    let d = new Date(millisec);
-    return `${d.getFullYear()}/${d.getMonth()+1}/${d.getDate()}  ${d.getHours()}:${d.getMinutes()}}`;
-}
+    // let d = new Date(millisec);
+    // return `${d.getFullYear()}/${d.getMonth()+1}/${d.getDate()}  ${d.toLocaleTimeString({hour:"2-digit"})}`;
+    return moment(millisec).format('YYYY/MM/DD  hh:mm:ss');
+};
 
 //  возращает строку из значений объекта params
-let createStrParams = (obj) => `${obj.status} => ${obj.progress}`
+let createStrParams = (obj) => `${obj.status} => ${obj.progress}`;
+
+// проверяет url на наличие http
+let checkUrl = (str) => ~str.indexOf('http://')? str : `http://${str}` ;
+
+// преобразует поле name что бы начиналось с большой буквы, а сотальные были будквы маленького регистра
+let firstChartToUpperCase = (str) =>  (str)? `${str[0].toUpperCase()}${str.slice(1).toLowerCase()}`: '';
 
 // Применяем метод map для модификации свойств
 let modifyNewArray = (arr) => {
-    return  arr.map(function(item) {
+    return  arr.map( (item) => {
         return{
-            url: `http://${item.url}`,
-            name: item.name[0].toUpperCase() + item.name.slice(1).toLowerCase(),
+            url: checkUrl(item.url),
+            name: firstChartToUpperCase(item.name),
             params: createStrParams(item.params),
             description: addThreePoint(item.description),
             date: setDateItem(item.date),
             isVisible: item.params.status
         };
     });
-}
+};
 
 let showIsVisible = (arr) => {
-    let array = arr.filter(function (item) {
-        return item.isVisible == true;
-    });
-    return array;
-}
+    return array = arr.filter((item) => item.isVisible );
+};
+
 let printResult = (result) => console.log( result );
 
 //Основная ф-ция запуска
 function transform() {
-    deleteElementArray(data, 1);
+    deleteElementArray(data, 6);
     let res = showIsVisible( modifyNewArray( createNewArray(data) ) );
     printResult(res);
 }
--- less_17/task_1.js
+++ less_17/task_1.js
@@ -1,63 +1,25 @@
 // 1. Из данного массива удалить значение «technics». Все остальное превратить в строку формата «foods, fruits…»
 // преобразование в строку выполнить с помощью одного метода.
-/*
-let goods = ['foods', 'fruits', 'technics', 'phones', 'computers', 'technics','technics'];
 
-while (true){
-    let indItem = goods.indexOf('technics');
-    if( indItem < 0) break;
-    goods.splice(indItem,1);
+let goods = ['foods', 'fruits', 'technics', 'phones', 'computers', 'technics','technics','phones'];
+
+let obj = {};
+for (var i = 0; i < goods.length; i++) {
+    let key = goods[i];
+    obj[key] = true;
 }
-console.log( goods.join(', ') );
-*/
-/*--------------------------------------------------------------------------------------------------*/
-// 2. Преобразовать текущую дату и время в понятный человеку формат: 08:05 01/01/2018. Используя шаблонные строки.
-/*
-let d = new Date();
-let currentDate = `${d.getHours()}:${d.getMinutes()} ${d.getDate()}/${d.getMonth()+1}/${d.getFullYear()}` ;
-console.log( currentDate );
-*/
-/*--------------------------------------------------------------------------------------------------*/
-// 3. Напишите функцию, которая возвращает расширение файла.
-// Например, getExt(«/home/user/project/script.js») вернет “js”. Функция должна принимать строку
-/*
-function getExt(str) {
-    let ind = str.indexOf('.')
-     console.log( str.slice(ind+1) ); //
+console.log( Object.keys(obj) );
+
+var arr = [];
+for (var i = 0; i < goods.length; i++) {
+
+    if(arr.indexOf(goods[i])<0 ) arr.push(goods[i]);
 }
-getExt('/home/user/project/script.js');
-*/
-/*--------------------------------------------------------------------------------------------------*/
-// 4. Напишите функцию, которая удаляет дубликаты из массива. Например, входной массив:
-// [1, 2, 2, 4, 5, 4, 7, 8, 7, 3, 6], массив который возвращает функция [1, 2, 4, 5, 7, 8, 3, 6]
+console.log( arr );
 
-// let arr = [1, 2, 2, 4, 5, 4, 7, 8, 7, 3, 6,6,6];
-// function uniqueArr(arr){
+var newArr = goods.filter(function (item, index, arr) {
+   return  index == arr.indexOf(item);
+});
 
+console.log( newArr );
 
-    /*Первый вариант*/
-    // let newArr = [];
-    // arr.forEach(function (item) {
-    //    if( newArr.indexOf(item) < 0 ) newArr.push(item);
-    // })
-    // return newArr;
-
-    /*Второй вариант*/
-    // var newArr = arr.filter(function (item, ind, arr) {
-    //     return ind == arr.indexOf(item)
-    // });
-    // return newArr;
-
-    /*Третий вариант*/
-    // let obj = {};
-    // arr.forEach(function (item) {
-    //     let key = item;
-    //     obj[key] = true;
-    // })
-    // return Object.keys(obj);
-
-
-
-} // end
-
-// console.log( uniqueArr(arr) );;
